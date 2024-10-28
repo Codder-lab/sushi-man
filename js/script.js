b53a63671a0ee0c4003f34826f4b0e457bf4a043
Suyash Potdar <84835929+Codder-lab@users.noreply.github.com>
2024-10-28 18:55:36 +0000

    add mobile-navigation

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ AOS.init({
     duration: 1000,
     offset: 100,
 });
+
+// JavaScript to toggle the mobile menu
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.getElementById("mobile-menu-icon");
+    const mobileMenu = document.getElementById("mobile-menu");
+  
+    menuIcon.addEventListener("click", function () {
+      mobileMenu.classList.toggle("show"); // Toggle the 'show' class on the mobile menu
+    });
+  });
